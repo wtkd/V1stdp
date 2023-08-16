b7826296cfb7130f34fe89d86d0ebcb58fffa3f2
ROCKTAKEY <rocktakey@gmail.com>
2023-08-16 10:37:01 +0900

    Remove unnecessary files

--- tool/image/image.hpp
+++
@@ -1,9 +0,0 @@
-#include <CLI/CLI.hpp>
-
-namespace tool::image {
-
-void inline setupImage(CLI::App &app) {
-  auto sub = app.add_subcommand("image", "Export, transform or treat image data");
-}
-
-} // namespace tool::image
