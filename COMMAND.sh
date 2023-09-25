86ea72e85348203d3b862d8c94eb4d412be2d49b
ROCKTAKEY <rocktakey@gmail.com>
2023-09-25 16:27:20 +0900

    Remove some unecessary files

--- COMMAND.sh
+++
@@ -1,1 +0,0 @@
-bsub -q long -g /stdp -W 60:00 ../stdp learn latconnmult 4.0 wie .6 wpenscale .33 altpmult .75 delayparam 5.0
--- COMMANDTEST.sh
+++
@@ -1,1 +0,0 @@
-bsub -q short -g /test -W 1:00 ../stdp test latconnmult 4.0 wie .6 wpenscale .33 altpmult .75 delayparam 5.0
