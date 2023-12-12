fd7bca84af323fa7f9e0d6c8fbb832682c30560a
ROCKTAKEY <rocktakey@gmail.com>
2023-12-12 21:00:18 +0900

    Add test for sparseness and smoothness

--- test/unit-test/main.cpp
+++ test/unit-test/main.cpp
@@ -94,3 +94,47 @@ TEST(evaluationFunction, secondPartialDerivativeRow2) {
     );
   }
 }
+
+TEST(evaluationFunction, sparseness) {
+  Eigen::ArrayXXd sparseMatrix(3, 3);
+  // clang-format off
+  sparseMatrix <<
+    0, 0, 1,
+    1, 0, 2,
+    3, 0, 2;
+  // clang-format on
+
+  Eigen::ArrayXXd denseMatrix(3, 3);
+  // clang-format off
+  denseMatrix <<
+    10, 20, 10,
+    20, 15, 23,
+    12, -30, 20;
+  // clang-format on
+  EXPECT_LT(
+      v1stdp::main::tool::analyze::exploreMaximum::evaluationFunction::sparseness(denseMatrix),
+      v1stdp::main::tool::analyze::exploreMaximum::evaluationFunction::sparseness(sparseMatrix)
+  );
+}
+
+TEST(evaluationFunction, smoothness) {
+  Eigen::ArrayXXd smoothMatrix(3, 3);
+  // clang-format off
+  smoothMatrix <<
+    0, 1, 2,
+    1, 2, 3,
+    2, 3, 2;
+  // clang-format on
+
+  Eigen::ArrayXXd roughMatrix(3, 3);
+  // clang-format off
+  roughMatrix <<
+    0, 3, 0,
+    1, 0, 2,
+    1, 3, 0;
+  // clang-format on
+  EXPECT_LT(
+      v1stdp::main::tool::analyze::exploreMaximum::evaluationFunction::smoothness(roughMatrix),
+      v1stdp::main::tool::analyze::exploreMaximum::evaluationFunction::smoothness(smoothMatrix)
+  );
+}
