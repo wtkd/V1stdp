b0936006b030dda8f322590ad4136a871edfd1f4
ROCKTAKEY <rocktakey@gmail.com>
2024-03-10 22:10:55 +0900

    Add threshold

--- src/main/tool/analyze/predict/include/predict.hpp
+++ src/main/tool/analyze/predict/include/predict.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <optional>
+
 #include <CLI/CLI.hpp>
 #include <Eigen/Dense>
 
@@ -8,7 +10,8 @@ namespace v1stdp::main::tool::analyze::predict {
 Eigen::ArrayXXd predictor(
     Eigen::MatrixXd const &feedforwardWeights,
     std::vector<Eigen::ArrayXX<std::int8_t>> const &images,
-    unsigned const edgeLength
+    unsigned const edgeLength,
+    std::optional<std::int32_t> const threshold
 );
 
 void setupPredict(CLI::App &app);
